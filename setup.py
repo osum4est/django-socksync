21641d902d3581c60dd037f296d81ce7501aa448
Forrest Jones <fjones-dev@8bitforest.com>
2019-06-27 22:56:13 -0600

    :wrench: Change name to django-socksync

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="socksync",
+    name="django-socksync",
     version="0.0.1",
     author="Forrest Jones",
     author_email="fjones-dev@8bitforest.com",
     description="Easily bind django model data with a websocket client.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/osum4est/socksync-django",
+    url="https://github.com/osum4est/django-socksync",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
